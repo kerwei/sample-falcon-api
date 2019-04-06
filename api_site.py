a611364222438e479eb49a96a1284a63f315c487
Ker Wei Khaw <kerwei84@gmail.com>
2019-04-06 20:43:53 +0800

    Create entry function added

--- api_site.py
+++ api_site.py
@@ -1,123 +1,114 @@
-# Standard Library
-import pdb  # For debugging
+from functools import wraps
+from datetime import datetime
 
-# Third party modules
 from flask import Flask
 from flask import request, render_template, redirect, url_for, flash
+from flask import abort, jsonify, flash, escape
 from flask import session as login_session
-from flask.ext.seasurf import SeaSurf
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.orm.exc import NoResultFound
-# # from sqlalchemy import desc
-# from oauth2client.client import flow_from_clientsecrets
-# from oauth2client.client import FlowExchangeError
-# import httplib2
-# import requests
 
-# Local custom modules
 import helpers
-# from helpers import valid_statetoken
-from database_setup import Base, User
+from database_setup import Base, Customer
 from dbfunctions import session
-# Blueprints
-from public_page import public_page
-from private_page import private_page
-from authenticate_page import authenticate_page
-from gconn_page import gconn_page
-from fbconn_page import fbconn_page
+
 
 # Starts the application and register the blueprints
 app = Flask(__name__)
-app.register_blueprint(public_page)
-app.register_blueprint(authenticate_page)
-app.register_blueprint(private_page)
-app.register_blueprint(gconn_page)
-app.register_blueprint(fbconn_page)
-# CSRF is taken care of by Flask-SeaSurf
-csrf = SeaSurf(app)
 
 
-# User Login
-# The module is placed here merely because it needs to invoke the csrf exempt
-# function. TODO: Propagate the csrf object through to the submodules and move
-# the login function to the authenticate_page module.
-# CSRF not relevant prior to user login
-@csrf.exempt
-@app.route('/login', methods=['POST', 'GET'])
-def loginSite():
+def parseUrlEntry(entry_string):
+    if '|' in entry_string:
+        name = entry_string.split('|')[0]
+        dob = datetime.strptime(entry_string.split('|')[1], '%d-%m-%Y')
+        return [name, dob]
+    else:
+        return []
+
+
+# Adds a new item to the catalog
+@app.route('/customer/add/<string:new_entry>', methods=['GET'])
+def newItem(new_entry):
+    data = parseUrlEntry(new_entry)
+    # Creates the record and saves it to the database
+    if data:
+        new_item = Customer(name=data[0], dob=data[1])
+        session.add(new_item)
+        session.commit()
+
+    return redirect(url_for('homePage'))
+
+
+# Edits the catalog item record
+@app.route('/customer/edit/<int:item_id>/<string:new_entry>', methods=['GET'])
+def editItem(item_id):
+    item = session.query(CatalogItem).filter_by(id=item_id).one()
+
+    # Redirects back to the main landing page if the record is not found
+    if not item:
+        flash("Invalid item. \
+            Please check that you have selected a valid item.")
+        return redirect(url_for('public_page.itemList'))
+
     if request.method == 'GET':
-        # Redirects the user back to the main landing page if he/she is already
-        # logged in
-        if 'userid' in login_session:
-            flash("You are already logged in!")
-            return redirect(url_for('public_page.itemList'))
-
-        return render_template('login.html')
+        return render_template('edititem.html', item=item)
 
     if request.method == 'POST':
-        # Retrieves the form details
-        user_name = request.form['name']
-        password = request.form['password']
-        # Checks that the required fields are not empty
-        nan_empty = helpers.nempty(username=user_name,
-            password=password)
-        # Throws the warning message if one of the fields is empty
-        if nan_empty is not True:
-            flash("Please ensure all fields are filled before submitting.")
-            return render_template('login.html', nan_message=nan_empty)
-        # Checks that the entered characters are valid
-        is_valid = helpers.valid(username=user_name,
-            password=password)
+        user_id = login_session['user_id']
 
-        if is_valid is True:
-            username = session.query(User).filter_by(name=user_name).all()
-
-            if username:
-                # Identical user names permitted by the site. Test the
-                # validity of the entered password by looping through the salt,
-                # generating the hash for each combination and check against
-                # the hashedpw stored in the db
-                for each in username:
-                    salt = each.salt
-                    hashedpw = helpers.make_pw_hash(user_name,
-                        password,
-                        salt).split('|')[0]
-                    try:
-                        # Probably unecessary to check the username again over
-                        # here given that the hash is generated from the
-                        # username as well. Only danger left is when two
-                        # users have the same name and password combination
-                        user = session.query(User).filter_by(
-                            hashedpw=hashedpw).one()
-                        if user:
-                            break
-                    except NoResultFound:
-                        user = None
-
-                if not user:
-                    flash("The entered password was incorrect. \
-                        Please try again.")
-                    return render_template('login.html', username=user_name)
-            else:
-                flash("User does not exist. Please check your username.")
-                return render_template('login.html', username=user_name)
-
-            # Sets the login session if login is successful
-            csrf_token = helpers.roast_chip(str(user.id) + user.name)
-            login_session['user_id'] = user.id
-            login_session['username'] = user.name
-            login_session['picture'] = user.picture
-            login_session['email'] = user.email
-            login_session['auth_type'] = "local"
-            flash("Welcome %s!" % user_name)
-            return redirect(url_for('public_page.itemList'))
+        # Checks that the item belongs to the rightful user
+        if user_id == item.user_id:
+            item.name = request.form['name']
+            item.price = request.form['price']
+            item.category = request.form['category']
+            item.description = request.form['description']
+            session.add(item)
+            session.commit()
+            flash("Item saved successfully!")
         else:
-            flash("Username/password not valid. Please re-enter.")
-            return render_template('login.html',
-                username=user_name,
-                err_username=is_valid['err_username'],
-                err_password=is_valid['err_password'])
+            flash("You are not authorized to edit this item!")
+            return redirect(url_for('public_page.viewCatalogItem',
+                category=item.category,
+                item_id=item.id))
+
+        return redirect(url_for('public_page.viewCatalogItem',
+            category=item.category,
+            item_id=item.id))
+
+
+# Deletes a catalog item
+@app.route('/customer/delete/<string:item_id>', methods=['GET'])
+def deleteItem(item_id):
+
+    item = session.query(Customer).filter_by(id=item_id).one()
+
+    # Redirects back to the main landing page if the record is not found
+    if not item:
+        flash("Invalid item. \
+            Please check that you have selected a valid item.")
+        return redirect(url_for('public_page.itemList'))
+
+    if request.method == 'GET':
+        return render_template('deleteitem.html', item=item)
+
+    if request.method == 'POST':
+        user_id = login_session['user_id']
+
+        # Checks that the user is the rightful owner of the item
+        if user_id == item.user.id:
+            session.delete(item)
+            session.commit()
+            flash("Item deleted!")
+        else:
+            flash("You are not authorized to delete this item!")
+            return redirect(url_for('public_page.viewCatalogItem',
+                category=item.category,
+                item_id=item.id))
+
+        return redirect(url_for('public_page.itemList'))
+
+# Retrieve a record
+@app.route('/', methods=['GET'])
+def homePage():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
